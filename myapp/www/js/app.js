88104506e5f81634089f726dbb08c00db8444691
suaq <s8186255@163.com>
2015-08-11 19:14:51 +0800

    myapp重建

--- myapp/www/js/app.js
+++ myapp/www/js/app.js
@@ -3,26 +3,55 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter',  ['ionic', 'starter.controllers', 'starter.services'])
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
     .run(function ($ionicPlatform) {
         $ionicPlatform.ready(function () {
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
-            //意思是不是隐藏键盘框，在有input控件的时候；
-            if (window.cordova && window.cordova.plugins.Keyboard) {
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
                 cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
+
             }
             if (window.StatusBar) {
-                StatusBar.styleDefault();
+                // org.apache.cordova.statusbar required
+                StatusBar.styleLightContent();
             }
         });
     })
+    .config(function ($ionicConfigProvider) {
+        $ionicConfigProvider.tabs.position('bottom');
+    })
     .config(function ($stateProvider, $urlRouterProvider) {
+
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
         $stateProvider
-            .state('tab',{
-                url:'tab',
+
+            // setup an abstract state for the tabs directive
+            .state('tab', {
+                url: '/tab',
+                abstract: true,
                 templateUrl: 'templates/tabs.html'
             })
+
+            // Each tab has its own nav history stack:
+
+            .state('tab.dash', {
+                url: '/dash',
+                views: {
+                    'tab-dash': {
+                        templateUrl: 'templates/tab-dash.html',
+                        controller: ''
+                    }
+                }
+            })
+
             .state('tab.chats', {
                 url: '/chats',
                 views: {
@@ -50,6 +79,22 @@ angular.module('starter',  ['ionic', 'starter.controllers', 'starter.services'])
                         controller: 'AccountCtrl'
                     }
                 }
-            });
+            })
+            .state('tab.chat', {
+                url: '/chat',
+                views: {
+                    'tab-chat': {
+                        templateUrl: 'templates/tab-chats.html',
+                        controller: 'ChatsCtrl'
+                    }
+                }
+            })
+            .state('login', {
+                url: '/login',
+                templateUrl: 'templates/login.html',
+                controller: 'LoginCtrl'
+            })
+        // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('/tab/dash');
-    })
+
+    });
