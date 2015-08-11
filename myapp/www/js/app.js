8a6aee6c636e11238cb39420c8103078447f0999
s8186255 <suanqi@gmail.com>
2015-08-12 00:34:11 +0800

    增加login

--- myapp/www/js/app.js
+++ myapp/www/js/app.js
@@ -89,10 +89,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
                     }
                 }
             })
-            .state('login', {
+            .state('tab.login', {
                 url: '/login',
-                templateUrl: 'templates/login.html',
-                controller: 'LoginCtrl'
+                views:{
+                    'tab-login':{
+                        templateUrl: 'templates/login.html',
+                        controller: 'LoginCtrl'
+
+                    }
+                },
             })
         // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('/tab/dash');
